93c767afc7bc61d9244a3eb11f202f9969829322
Alex Xiang <axiang6912@gmail.com>
2021-05-05 20:57:33 -0400

    Separated files

--- lib/cards.dart
+++ lib/cards.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nutrigorithm/ui/widgets/recipe_card.dart';
+import 'package:nutrigorithm/model/recipe.dart';
+
 class SecondRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -19,12 +22,15 @@ class SecondRoute extends StatelessWidget {
               ButtonTheme(
                 minWidth: 350.0,
                 height: 200.0,
-                child: RaisedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text("test"),
-                ),
+                child:  RecipeCard(Recipe("Recipe", 'https://images.unsplash.com/photo-1506802913710-40e2e66339c9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c8ffc5bbb3719b5a46ee703acb0a0ac5&auto=format&fit=crop&w=634&q=80',
+                    RecipeType.breakfast,
+                    [
+                      '100g of fresh blueberries',
+                      '200g of plain yoghurt',
+                      '100g of milk',
+                      '1 banana',
+                    ]),
+                )
               ),
               Padding (
                 padding: EdgeInsets.symmetric(vertical: 8.0),
